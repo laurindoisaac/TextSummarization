57fc21c94c36a51ec5837db55f2e2c8650a48c10
Lauri <116688165+laurindoisaac@users.noreply.github.com>
2025-08-10 12:33:57 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/textsummarization/main.go
@@ -0,0 +1,23 @@
+// cmd/textsummarization/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "textsummarization/internal/textsummarization"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := textsummarization.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
